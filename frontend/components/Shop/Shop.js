74b3709d55c313a23b06ea580ab2be8311c3df1e
amafigu <amadeusfigue@gmail.com>
2023-05-12 23:17:10 +0200

    Products in shop component

--- frontend/components/Shop/Shop.js
+++ frontend/components/Shop/Shop.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Product from './Product';
 
 const Shop = () => {
+  const products = [
+    { name: 'Lavender Oil', image: '/path/to/lavender.jpg', size: 5, price: 10 },
+    { name: 'Peppermint Oil', image: '/path/to/peppermint.jpg', size: 15, price: 15 },
+    // add more products here
+  ];
+
   return (
     <div>
       <h1>Our Shop</h1>
-      <p>Coming soon: Browse our selection of essential oils here.</p>
+      {products.map((product, index) => (
+        <Product 
+          key={index} 
+          name={product.name} 
+          image={product.image} 
+          size={product.size} 
+          price={product.price} 
+        />
+      ))}
     </div>
   );
 };
