2747d296a7bc171454639fc7565de360710f5e5a
amafigu <amadeusfigue@gmail.com>
2023-05-16 09:33:53 +0200

    minor adjustments in server and shop component

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const productRoutes = require('./routes/products');
 
 app.use(cors());
 app.use(express.json());
-
-const productRoutes = require('./routes/products');
+app.use('/api/products', productRoutes);
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port ${process.env.PORT || 3001}.`);
