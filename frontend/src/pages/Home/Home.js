d5e6086e0297937e4d73e9b6d3080ea0435dc8a1
amafigu <amadeusfigue@gmail.com>
2023-06-05 15:29:37 +0200

    add product slider and format code

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -11,14 +11,26 @@ const Home = () => {
     <div className={styles.homeContainer}>
       <Carousel />
       <div className={styles.teaserText}>
-      <h1 className={styles.hometitle}>{translate.pages.welcome.title}</h1>
-      <p className={styles.homesubtitle}>{translate.pages.welcome.teaserSentence}</p>
-      <p className={styles.homesubtitle}>{translate.pages.welcome.teaserSubSentenceOne}</p>
-      <p className={styles.homesubtitle}>{translate.pages.welcome.teaserSubSentenceTwo}</p>
-      <p className={styles.homesubtitle}>{translate.pages.welcome.teaserSubSentenceThree}</p>
-      <p className={styles.homesubtitle}>{translate.pages.welcome.teaserSubSentenceFour}</p>
+        <h1 className={styles.hometitle}>{translate.pages.welcome.title}</h1>
+        <p className={styles.homesubtitle}>
+          {translate.pages.welcome.teaserSentence}
+        </p>
+        <p className={styles.homesubtitle}>
+          {translate.pages.welcome.teaserSubSentenceOne}
+        </p>
+        <p className={styles.homesubtitle}>
+          {translate.pages.welcome.teaserSubSentenceTwo}
+        </p>
+        <p className={styles.homesubtitle}>
+          {translate.pages.welcome.teaserSubSentenceThree}
+        </p>
+        <p className={styles.homesubtitle}>
+          {translate.pages.welcome.teaserSubSentenceFour}
+        </p>
       </div>
-      <ProductsSlider />
+      <div className={styles.productSliderContainer}>
+        <ProductsSlider />
+      </div>
     </div>
   )
 }
