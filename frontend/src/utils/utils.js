825d24ebb3074835057f59bc6dc5ed9686a16777
amafigu <amadeusfigue@gmail.com>
2023-07-25 13:31:05 +0200

    extract addOneProductToCart function into utils and adjust imports

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -4,3 +4,7 @@ export const titleCase = (str, separator) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ")
 }
+
+export const addOneProductToCart = (product, contextFunction) => {
+  contextFunction(product, 1)
+}
